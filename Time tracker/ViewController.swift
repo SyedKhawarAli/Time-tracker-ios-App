6cdc437db1156d115ef200763f3103a012ecd251
Khawar Ali <khawarali5@gmail.com>
2018-05-04 10:32:53 +0500

    Created time tracker project

--- Time tracker/ViewController.swift
+++ Time tracker/ViewController.swift
@@ -10,15 +10,64 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var eggTime  = 210
+    
+    var timer = Timer()
+    
+    @IBOutlet weak var timeText: UILabel!
+    
+    @IBAction func onPauseClick(_ sender: Any) {
+        timer.invalidate()
+    }
+    @IBAction func onResumeClick(_ sender: Any) {
+        if (!timer.isValid){
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.timerProcess), userInfo: nil, repeats: true)
+        }
+    }
+    
+    @IBAction func onResetClick(_ sender: Any) {
+        eggTime = 210
+        timeText.text = String(eggTime)
+    }
+    
+    @IBAction func onMinus10Click(_ sender: Any) {
+        if(eggTime-10>=0){
+            eggTime -= 10
+        }else{
+            
+        }
+        timeText.text = String(eggTime)
+    }
+    
+    @IBAction func onPlus10Click(_ sender: Any) {
+        eggTime += 10
+        timeText.text = String(eggTime)
+    }
+    
+    @objc func timerProcess() {
+        if(eggTime-1>0){
+            eggTime -= 1
+            timeText.text = String(eggTime)
+        }else{
+            timer.invalidate()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        timeText.text = String(eggTime)
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
